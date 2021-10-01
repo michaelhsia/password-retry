8c6457a8fbf30ee91ba3dd1f97e94c217fdb7d07
Michael Hsia <michaelhsia8716@gmail.com>
2021-10-01 17:05:32 +0800

    final version

--- password_reset.py
+++ password_reset.py
@@ -2,9 +2,13 @@ password = 'a123456'
 n = 3 #剩餘機會
 while n > 0:
     pwd = input('請輸入密碼: ')
+    n = n - 1
     if pwd == password:
         print('登入成功!')
         break #逃出迴圈
     else:
-        n = n - 1
-        print('密碼錯誤！還有', n, '次機會')
+        print('密碼錯誤!')
+        if n > 0:
+            print('還有', n, '次機會')
+        else:
+            print('要鎖帳號了喔！')
